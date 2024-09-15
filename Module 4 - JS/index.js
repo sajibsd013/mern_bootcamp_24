c801deec40e071dd3e1bbb8072ca6f3e2be5192f
Sajib Sutradhar <sajibsd013@gmail.com>
2024-09-15 21:52:29 +0600

    ***

---
+++ Module 4 - JS/index.js
@@ -0,0 +1,42 @@
+// var name = "Sajib"
+// var id = 19010
+// var cg = 3.25
+
+// int 12
+// float 12.212
+// String "Sajib"
+
+// let & const 
+ 
+// 1st 
+const _name = "Sajib"
+const id = 19901
+let cg = 3
+
+// 2st 
+cg = 3.5
+
+// console.log(_name, id, cg);
+
+let x = 10
+let y = 4
+
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x%y);
+
+console.log(5**5);
+// x = x + y
+x+=y
+z = true
+console.log(x, y,z);
+
+
+
+// home work 
+// id vs class 
+// let vs const vs var 
+// None vs Null vs undefined
+
